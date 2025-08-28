3a1d71c2b43cb95844de27d112eff1e84c9d4553
Jean <jeannguilherme30@gmail.com>
2025-08-28 14:36:20 -0300

    feat/adicionado servidor Fastify e rota inicial /

--- server.js
+++ server.js
@@ -1,9 +1,21 @@
-import { createServer } from 'node:http'
+// import { createServer } from 'node:http'
 
-const server = createServer((request, response) =>{
-    //console.log('oi');
-    response.write('oiiii');
-    return response.end();
+// const server = createServer((request, response) =>{
+//     //console.log('oi');
+//     response.write('oiiii');
+//     return response.end();
+// })
+
+// server.listen(3333);
+
+import { fastify } from "fastify";
+
+const server = fastify();
+
+server.get('/', () =>{
+    return 'Hello world'
 })
 
-server.listen(3333);
+server.listen({
+    port : 3333,
+});
