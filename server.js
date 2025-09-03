0945fd766dfe1a67de63b8a50b76908664387bbf
Jean <jeannguilherme30@gmail.com>
2025-09-03 19:10:24 -0300

    adicionado query parameter

--- server.js
+++ server.js
@@ -18,8 +18,16 @@ server.post('/videos', (request, reply) =>{
     return reply.status(201).send();
 })
 
-server.get('/videos', () =>{
-    const videos = database.list()
+// server.get('/videos', () =>{
+//     const videos = database.list()
+
+//     return videos
+// })
+
+server.get('/videos', (request) => {
+    const search = request.query.search
+    
+    const videos = database.list(search);
 
     return videos
 })
