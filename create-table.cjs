e7d3dabf3f9cf9077438e6112b9ec13a0c52cea1
Jean <jeannguilherme30@gmail.com>
2025-09-03 22:48:16 -0300

    CRUD funcionando com banco neon

--- create-table.cjs
+++ create-table.cjs
@@ -4,9 +4,16 @@ require("dotenv").config();
 const sql = neon(process.env.DATABASE_URL);
 
 (async () => {
+
+  // await sql`
+  //   DROP TABLE IF EXISTS videos
+  // ;`.then(() => {
+  //   console.log('Tabela apagada')
+  // })
+
   await sql`
     CREATE TABLE IF NOT EXISTS videos (
-      id BIGSERIAL PRIMARY KEY,
+      id TEXT PRIMARY KEY,
       title TEXT NOT NULL,
       description TEXT,
       duration INTEGER
--- server.cjs
+++ server.cjs
@@ -1,15 +1,15 @@
-import { fastify } from "fastify";
-import { DatabaseMemory } from "./database-memory.js";
+const { fastify } = require("fastify");
+const { DatabasePostgres } = require("./database-postgres.cjs");
 
 const server = fastify();
-const database = new DatabaseMemory()
-
+// const database = new DatabaseMemory()
+const database = new DatabasePostgres();
 //Request Body
 
-server.post('/videos', (request, reply) =>{
+server.post('/videos', async (request, reply) =>{
     const {title , description, duration} = request.body;
 
-    database.create({
+    await database.create({
         title,
         description,
         duration
@@ -24,19 +24,19 @@ server.post('/videos', (request, reply) =>{
 //     return videos
 // })
 
-server.get('/videos', (request) => {
+server.get('/videos', async (request) => {
     const search = request.query.search
     
-    const videos = database.list(search);
+    const videos = await database.list(search);
 
     return videos
 })
 
-server.put('/videos/:id', (request, reply) => {   
+server.put('/videos/:id', async (request, reply) => {   
     const videoId = request.params.id
     const {title , description, duration} = request.body;
 
-    database.update(videoId, {
+    await database.update(videoId, {
         title,
         description,
         duration
@@ -45,10 +45,10 @@ server.put('/videos/:id', (request, reply) => {
     return reply.status(204).send();
 })
 
-server.delete('/videos/:id', (request, reply) => {
+server.delete('/videos/:id', async (request, reply) => {
     const videoId = request.params.id
 
-    database.delete(videoId);
+    await database.delete(videoId);
 
     return reply.status(204).send();
 })
